dd091ff942fb794f117f2317372f33476fcdcb60
LAPTOP-A1ECJ9D7\60238 <lirui@21tb.com>
2020-04-16 18:11:01 +0800

    Iterate

---
+++ src/main/java/com/flink/student/Iterate.java
@@ -0,0 +1,40 @@
+package com.flink.student;
+
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.IterativeStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class Iterate {
+    public static void main(String[] args) throws Exception{
+        //无界流处理环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStream<Long> someIntegers = env.generateSequence(0, 1000);
+
+        IterativeStream<Long> iteration = someIntegers.iterate();
+
+        DataStream<Long> minusOne = iteration.map(new MapFunction<Long, Long>() {
+            @Override
+            public Long map(Long value) throws Exception {
+                return value - 1 ;
+            }
+        });
+
+        DataStream<Long> stillGreaterThanZero = minusOne.filter(new FilterFunction<Long>() {
+            @Override
+            public boolean filter(Long value) throws Exception {
+                return (value > 0);
+            }
+        });
+
+        iteration.closeWith(stillGreaterThanZero);
+
+        DataStream<Long> lessThanZero = minusOne.filter(new FilterFunction<Long>() {
+            @Override
+            public boolean filter(Long value) throws Exception {
+                return (value <= 0);
+            }
+        });
+    }
+}
