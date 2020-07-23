04c17404845fffa45e60e88681a90994bd0c86b1
LAPTOP-A1ECJ9D7\60238 <602389275@qq.com>
2020-07-24 00:08:22 +0800

    默认更改列表

--- src/main/java/com/flink/student/ProcessTimeJob.java
+++ src/main/java/com/flink/student/ProcessTimeJob.java
@@ -1,9 +1,6 @@
 package com.flink.student;
 
-import org.apache.flink.api.common.state.MapState;
-import org.apache.flink.api.common.state.MapStateDescriptor;
-import org.apache.flink.api.common.state.ValueState;
-import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.state.*;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.configuration.Configuration;
@@ -12,34 +9,41 @@ import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.TimerService;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks;
+import org.apache.flink.streaming.api.functions.AssignerWithPunctuatedWatermarks;
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.apache.flink.streaming.api.functions.windowing.ProcessAllWindowFunction;
+import org.apache.flink.streaming.api.watermark.Watermark;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
 
+import javax.annotation.Nullable;
 import java.util.*;
 
 public class ProcessTimeJob {
-    private static List<Integer> data = Lists.newArrayList(1,2,3,4,5);
+    private static List<Integer> data = Lists.newArrayList(1, 2, 3, 4, 5);
+
     public static void main(String[] args) throws Exception {
         state();
     }
+
     private static void state() throws Exception {
         StreamExecutionEnvironment e = StreamExecutionEnvironment.getExecutionEnvironment();
-        DataStreamSource<Tuple2<Integer,Long>> dataStreamSource = e.addSource(new SourceFunction<Tuple2<Integer,Long>>() {
+        DataStreamSource<Tuple2<Integer, Long>> dataStreamSource = e.addSource(new SourceFunction<Tuple2<Integer, Long>>() {
             private volatile boolean stop = false;
             private Random random = new Random();
 
             @Override
-            public void run(SourceContext<Tuple2<Integer,Long>> ctx) throws Exception {
+            public void run(SourceContext<Tuple2<Integer, Long>> ctx) throws Exception {
                 int i = 0;
                 while (!stop && i < data.size()) {
                     ctx.collectWithTimestamp(
-                            Tuple2.of(data.get(i++),System.currentTimeMillis()),
+                            Tuple2.of(data.get(i++), System.currentTimeMillis()),
                             System.currentTimeMillis() - random.nextInt(500));
                     Thread.sleep(200);
                 }
@@ -51,32 +55,102 @@ public class ProcessTimeJob {
             }
         }).setParallelism(1);
         e.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
-        KeyedStream<Tuple2<Integer,Long>, Integer> integerIntegerKeyedStream = dataStreamSource.keyBy(v -> v.f0 % 2);
-        integerIntegerKeyedStream
-                .windowAll(TumblingEventTimeWindows.of(Time.seconds(10)))
-                .process(new ProcessAllWindowFunction<Tuple2<Integer,Long>, Tuple2<Integer,Long>, TimeWindow>() {
+        KeyedStream<Tuple2<Integer, Long>, Integer> integerIntegerKeyedStream = dataStreamSource.keyBy(v -> v.f0 % 2);
+        integerIntegerKeyedStream.windowAll(TumblingEventTimeWindows.of(Time.seconds(4)))
+                .process(new ProcessAllWindowFunction<Tuple2<Integer, Long>, Tuple2<Integer, Long>, TimeWindow>() {
                     MapState<Long, Integer> mapState;
+
+
                     @Override
                     public void process(Context context, Iterable<Tuple2<Integer, Long>> elements, Collector<Tuple2<Integer, Long>> out) throws Exception {
+                        Iterator<Tuple2<Integer, Long>> iterator = elements.iterator();
+                        iterator
+                        out.collect();
+                    }
+
+                    @Override
+                    public void open(Configuration parameters) throws Exception {
+                        super.open(parameters);
+                        MapStateDescriptor<Long, Integer> mapStateDescriptor = new MapStateDescriptor<>(
+                                "sum",
+                                Types.LONG,
+                                Types.INT
+                        );
+                        mapState = getRuntimeContext().getMapState(mapStateDescriptor);
+                    }
+
+                    @Override
+                    public void clear(Context context) throws Exception {
+                        super.clear(context);
+                        mapState.clear();
+                    }
+                });
+    }
+
+/*    private static void event() throws Exception {
+        StreamExecutionEnvironment e = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStreamSource<Tuple2<Integer, Long>> dataStreamSource = e.addSource(new SourceFunction<Tuple2<Integer, Long>>() {
+            private volatile boolean stop = false;
+            private Random random = new Random();
+
+            @Override
+            public void run(SourceContext<Tuple2<Integer, Long>> ctx) throws Exception {
+                int i = 0;
+                while (!stop && i < data.size()) {
+                    ctx.collectWithTimestamp(
+                            Tuple2.of(data.get(i++), System.currentTimeMillis()),
+                            System.currentTimeMillis() - random.nextInt(500));
+                    Thread.sleep(200);
+                }
+            }
+
+            @Override
+            public void cancel() {
+                stop = true;
+            }
+        }).setParallelism(1);
+        e.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+        KeyedStream<Tuple2<Integer, Long>, Integer> integerIntegerKeyedStream = dataStreamSource.keyBy(v -> v.f0 % 2);
+        SingleOutputStreamOperator<Tuple2<Integer, Long>> tuple2SingleOutputStreamOperator = integerIntegerKeyedStream.
+                assignTimestampsAndWatermarks(new AssignerWithPunctuatedWatermarks<Tuple2<Integer, Long>>() {
+
+                    @Nullable
+                    @Override
+                    public Watermark checkAndGetNextWatermark(Tuple2<Integer, Long> lastElement, long extractedTimestamp) {
+                        return null;
+                    }
+
+                    @Override
+                    public long extractTimestamp(Tuple2<Integer, Long> element, long previousElementTimestamp) {
+                        return 0;
+                    }
+                });
+        tuple2SingleOutputStreamOperator.
+                process(new KeyedProcessFunction<Integer, Tuple2<Integer, Long>, Tuple2<Integer, Long>>() {
+                    @Override
+                    public void processElement(Tuple2<Integer, Long> value, Context ctx, Collector<Tuple2<Integer, Long>> out) throws Exception {
 
                     }
 
-            @Override
-            public void processElement(Tuple2<Integer,Long> value, Context ctx, Collector<Tuple2<Integer,Long>> out) throws Exception {
-                TimerService timerService = ctx.timerService();
-                long l = timerService.currentWatermark();
-            }
+                    MapState<Long, Integer> mapState;
 
-            @Override
-            public void open(Configuration parameters) throws Exception {
-                super.open(parameters);
-                MapStateDescriptor<Long, Integer> mapStateDescriptor = new MapStateDescriptor<Long, Integer>(
-                        "sum",
-                        Types.LONG,
-                        Types.INT
-                );
-                getRuntimeContext().getMapState(mapStateDescriptor);
-            }
-        });
-    }
+
+                    @Override
+                    public void open(Configuration parameters) throws Exception {
+                        super.open(parameters);
+                        MapStateDescriptor<Long, Integer> mapStateDescriptor = new MapStateDescriptor<>(
+                                "sum",
+                                Types.LONG,
+                                Types.INT
+                        );
+                        mapState = getRuntimeContext().getMapState(mapStateDescriptor);
+                    }
+
+                    @Override
+                    public void clear(Context context) throws Exception {
+                        super.clear(context);
+                        mapState.clear();
+                    }
+                });
+    }*/
 }
